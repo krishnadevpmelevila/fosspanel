0da72ced21daeea5ea984e6343da30389e9ac0c3
Krishnadev P Melevila <krishnadev@kali>
2022-06-10 23:20:29 +0530

    first commit

---
+++ models/activation.js
@@ -0,0 +1,21 @@
+// create model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// create schema
+var activationSchema = new Schema({
+    name: String,
+    email: String,
+    sem: String,
+    branch: String,
+    phone: Number,
+    activated: Boolean,default:false,
+    paid: Boolean,default:false,
+    activationCode: String,
+    discordid: String,default:false,
+    created_at: Date,
+    updated_at: Date
+});
+// create model
+var Activation = mongoose.model('Activation', activationSchema);
+// export model
+module.exports = Activation;
